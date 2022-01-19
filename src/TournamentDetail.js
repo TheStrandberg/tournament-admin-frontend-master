804eb7de04773318980f39e762b2d536caa67b2b
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-01-19 16:16:03 +0100

    UseEffect fetches data

--- src/TournamentDetail.js
+++ src/TournamentDetail.js
@@ -4,29 +4,37 @@ import { useState, useEffect } from 'react';
 import DisplayTournaments from './DisplayTournaments';
 
 function TournamentDetail() {
-    useEffect(( ) => {
-        GetTournament();
-    }, [])
     let { id } = useParams();
     console.log(id);
 
     const [tournament, setTournament] = useState({});
 
-    async function GetTournament() {
-        const response = await fetch ("https://localhost:44335/api/Tournaments/" + id, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-            })
-            const tournamentArray = await response.json();
-            console.log(tournamentArray);
-            setTournament(tournamentArray);  
-    }
+    useEffect(() => {
+        async function getTournament() {
+            const response = await fetch ("https://localhost:44335/api/Tournaments/" + id, {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'},
+                })
+                const tournamentArray = await response.json();
+                console.log(tournamentArray);
+                setTournament(tournamentArray);  
+        }
+        getTournament();
+      }, []);    
 
     return (
         <div>
         {/* <Link to="/"><button> Back </button></Link> */}
         <h1>Tournament Detail</h1>
-        <h1>{tournament[id].tournamentName}</h1>
+        <h1>{tournament.tournamentName}</h1>
+        <h2>{tournament.game.title}</h2>
+        <p>{tournament.description}</p>
+        <h3>{tournament.eventTime}</h3>   
+        <h4>{tournament.venue.venueName}</h4> 
+        <h4>{tournament.venue.address.street}</h4>
+        <h4>{tournament.venue.address.city}</h4>
+        <h4>{tournament.venue.address.country}</h4>
+
         {/* <h2>{tournamentState[id].game.title}</h2>
         <p>{tournamentState[id].description}</p>
         <h3>{tournamentState[id].eventTime}</h3>   
