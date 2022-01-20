d8b75b56fb7b8e27a9621eac4da846a7becd1e15
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-01-20 12:48:44 +0100

    enter

--- src/DisplayTournaments.js
+++ src/DisplayTournaments.js
@@ -4,13 +4,12 @@ import { Link } from 'react-router-dom';
 import "./App.css"
 
 function DisplayTournaments( { tournaments }) {
-  
+
   return (
-      <div className="grid">
+      <div className="">
       {tournaments.map((tournament) => {
         return <Link to={`/${tournament.id}`} key={tournament.id}> 
         <TournamentComponent tournamentName={tournament.tournamentName} 
-        description={tournament.description} 
         eventTime={tournament.eventTime.toString().slice(0, 10)} 
         venue={tournament.venue}
         game={tournament.game}/>
